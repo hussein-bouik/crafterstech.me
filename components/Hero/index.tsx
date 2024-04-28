ae02779ce578be6714b417b56630883d1ccc9772
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-04-28 14:32:49 +0100

    Update Hero component content in index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -16,21 +16,17 @@ const Hero = () => {
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
               <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
-                🔥 Solid - A Complete SaaS Web Template
+                🔥 CraftersTech : scale your business
               </h4>
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
-                Free Next.js Template for {"   "}
+                {"   "}
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
                   SaaS
                 </span>
               </h1>
               <p>
-                Solid Pro - Packed with all the key integrations you need for
-                swift SaaS startup launch, including - Auth, Database, Sanity
-                Blog, Essential Components, Pages and More. Built-winth -
-                Next.js 13, React 18 and TypeScript.
+                CraftersTech is a startup that provides digital solutions. We have a team of expert people who can develop websites, design t-shirts, logos, sites, and business cards. With our expertise, we can help scale your business and bring your ideas to life.
               </p>
-
               <div className="mt-10">
                 <form onSubmit={handleSubmit}>
                   <div className="flex flex-wrap gap-5">
@@ -49,13 +45,11 @@ const Hero = () => {
                     </button>
                   </div>
                 </form>
-
                 <p className="mt-5 text-black dark:text-white">
-                  Try for free no credit card required.
+                  Try for free, no credit card required.
                 </p>
               </div>
             </div>
-
             <div className="animate_right hidden md:w-1/2 lg:block">
               <div className="relative 2xl:-mr-7.5">
                 <Image
