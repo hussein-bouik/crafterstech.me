c0489c5926ac4cea3969cc2ef7a532d0ba484a75
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-04-28 14:35:48 +0100

    Update Hero component content to use "CraftersTech" in index.tsx

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
                 {"   "}
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
-                  SaaS
+                  CraftersTech
                 </span>
               </h1>
               <p>
