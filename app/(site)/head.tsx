1ea3078817e6e404d32ed079e10a30dbdfb43334
husseinbouik <116066389+husseinbouik@users.noreply.github.com>
2024-04-28 14:25:58 +0100

    Update page metadata in head.tsx

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title> Solid | Next.js Template for Startup and SaaS</title>
+      <title>The craftersTech : A digital solution agency for your business </title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Built with Next.js and TypeScript" />
+      <meta name="description" content="Crafeterstech" />
       <link rel="icon" href="/images/favicon.ico" />
     </>
   );
